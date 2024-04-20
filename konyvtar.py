fc9eaca358f1d4dda56a824ee4621cf501dfa3d6
lirdce <lirdce1@gmail.com>
2024-04-20 14:51:40 +0200

    pushy

---
+++ konyvtar.py
@@ -0,0 +1,24 @@
+class Konyv:
+
+    lokacio = "én könyvtáram"
+    def __init__(self,konyv_cim,konyv_ar = 500):
+        self.ar = konyv_ar
+        self.cim = konyv_cim
+    def kiir(self):
+        print(f"A könyv címe: {self.cim} és ára: {self.ar}")
+    def aratNovel(self,ertek):
+        self.ar += ertek;
+
+my_konyv = Konyv("haború és béke",200)
+
+print(my_konyv.ar)
+
+other_konyv = Konyv("Működj és működik",6000)
+
+print(other_konyv.cim)
+
+print(my_konyv.lokacio)
+
+my_konyv.kiir()
+
+my_konyv.aratNovel(20)
